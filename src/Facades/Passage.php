3074e599d130c792bbeb2a44550be4c82b3f1ec8
Brian Faust <hello@basecode.sh>
2023-05-23 04:33:50 +0300

    fix: add missing method hints for facade

--- src/Facades/Passage.php
+++ src/Facades/Passage.php
@@ -4,8 +4,14 @@ declare(strict_types=1);
 
 namespace BombenProdukt\Passage\Facades;
 
+use BombenProdukt\Passage\Authentication\Authentication;
+use BombenProdukt\Passage\Management\Management;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static Authentication authentication()
+ * @method static Management     management()
+ */
 final class Passage extends Facade
 {
     protected static function getFacadeAccessor(): string
